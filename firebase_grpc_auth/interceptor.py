3a8ec0e5f939a5975a6e90b536f94438238bab82
DESKTOP-LKZ\laure <laurenskz@gmail.com>
2025-03-26 15:07:02 +0100

    constructor dependencies

--- firebase_grpc_auth/interceptor.py
+++ firebase_grpc_auth/interceptor.py
@@ -5,15 +5,22 @@ from firebase_admin import credentials, auth
 from grpc import ServerInterceptor
 
 
+def auth_interceptor_from_env_variables():
+    return FirebaseAuthInterceptor(
+        auth_enabled=os.getenv("ENABLE_JWT_AUTH", "false").lower() == "true",
+        cred_path=os.getenv("FIREBASE_SERVICE_ACCOUNT_PATH")
+    )
+
+
 class FirebaseAuthInterceptor(ServerInterceptor):
-    def __init__(self):
+    def __init__(self, cred_path: str, auth_enabled: bool):
         """
         gRPC Interceptor that enforces Firebase Authentication if ENABLE_JWT_AUTH is set.
         It uses FIREBASE_SERVICE_ACCOUNT_PATH if available, otherwise falls back to
         Google Application Default Credentials.
         """
-        self.auth_enabled = os.getenv("ENABLE_JWT_AUTH", "false").lower() == "true"
-        self.cred_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_PATH")
+        self.auth_enabled = auth_enabled
+        self.cred_path = cred_path
 
         if self.auth_enabled:
             if not firebase_admin._apps:
@@ -26,6 +33,7 @@ class FirebaseAuthInterceptor(ServerInterceptor):
     def _create_abort(self, reason: str):
         def abort(ignored_request, context):
             context.abort(grpc.StatusCode.UNAUTHENTICATED, reason)
+
         return abort
 
     def intercept_service(self, continuation, handler_call_details):
--- tests/test_interceptor.py
+++ tests/test_interceptor.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from firebase_grpc_auth.interceptor import FirebaseAuthInterceptor
+from firebase_grpc_auth.interceptor import FirebaseAuthInterceptor, auth_interceptor_from_env_variables
 
 
 class TestFirebaseAuthInterceptor(unittest.TestCase):
@@ -9,12 +9,12 @@ class TestFirebaseAuthInterceptor(unittest.TestCase):
 
     def test_auth_disabled(self):
         os.environ["ENABLE_JWT_AUTH"] = "false"
-        interceptor = FirebaseAuthInterceptor()
+        interceptor = auth_interceptor_from_env_variables()
         self.assertFalse(interceptor.auth_enabled)
 
     def test_auth_enabled(self):
         os.environ["ENABLE_JWT_AUTH"] = "true"
-        interceptor = FirebaseAuthInterceptor()
+        interceptor = auth_interceptor_from_env_variables()
         self.assertTrue(interceptor.auth_enabled)
 
 

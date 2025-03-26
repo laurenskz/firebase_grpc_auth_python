b8be5b08a434c0ac14e79ed35582f22aeb03ed23
DESKTOP-LKZ\laure <laurenskz@gmail.com>
2025-03-26 15:12:22 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="firebase-grpc-auth",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "firebase-admin",
